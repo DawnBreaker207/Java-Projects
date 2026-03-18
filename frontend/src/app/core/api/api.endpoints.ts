e707def22e189ab5775409e36aac7fcb52ef5e32
DawnBreaker <tunganhngo207@gmail.com>
2026-03-18 19:32:30 +0700

    feat: update Angular to React

--- frontend/src/app/core/api/api.endpoints.ts
+++
@@ -1,5 +0,0 @@
-import { environment } from '@env/environment';
-
-const API_BASE = environment.apiUrl;
-
-export const ApiEndpoints = {};
--- frontend/src/app/features/error/error/error-404.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-error',
-  imports: [],
-  templateUrl: './error-404.component.html',
-  styleUrl: './error-404.component.css',
-})
-export class Error404Component {}
