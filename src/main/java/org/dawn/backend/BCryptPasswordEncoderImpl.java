a79366e3ae785b2392a099cdb8eb0b6d32f2573d
DawnBreaker <tunganhngo207@gmail.com>
2026-04-15 15:58:03 +0700

    feat: update hash password config

---
+++ src/main/java/org/dawn/backend/BCryptPasswordEncoderImpl.java
@@ -0,0 +1,19 @@
+package org.dawn.backend;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class BCryptPasswordEncoderImpl implements PasswordEncoder {
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return BCrypt.hashpw(rawPassword.toString(), BCrypt.gensalt(12));
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        try {
+            return BCrypt.checkpw(rawPassword.toString(), encodedPassword);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/org/dawn/backend/PasswordEncoder.java
@@ -0,0 +1,7 @@
+package org.dawn.backend;
+
+public interface PasswordEncoder {
+    String encode(CharSequence rawPassword);
+
+    boolean matches(CharSequence rawPassword, String encodedPassword);
+}
